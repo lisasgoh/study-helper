cf84d1d40504056db00913d71343280681d2f35f
GoldenCorgi <42860714+GoldenCorgi@users.noreply.github.com>
2020-01-17 18:14:52 +0800

    fix

--- screens/Slides.js
+++ screens/Slides.js
@@ -5,27 +5,27 @@ const cz2002 = [
     {
     "week": "1",
     "transcript": {
-        "0:00:00": "she sells sea shells by the sea shore",
-        "0:00:10": "pineapple pen",
-        "0:00:20": "peter piper picked a peck of pickled pepper"
+        "0:00:00": ": she sells sea shells by the sea shore",
+        "0:00:10": ": pineapple pen",
+        "0:00:20": ": peter piper picked a peck of pickled pepper"
     },
     "slides": {
-        "Slide 1": "0:00:00",
-        "Slide 2": "0:00:50",
-        "Slide 3": "0:01:30"
+        "Slide 1": ": 0:00:00",
+        "Slide 2": ": 0:00:50",
+        "Slide 3": ": 0:01:30"
     }
     },
     {
     "week": "2",
     "transcript": {
-        "0:00:00": "elmo's world",
-        "0:00:10": "you should see me in a crown",
-        "0:00:20": "sample text"
+        "0:00:00": ": elmo's world",
+        "0:00:10": ": you should see me in a crown",
+        "0:00:20": ": sample text"
     },
     "slides": {
-      "Slide 1": "0:00:00",
-      "Slide 2": "0:00:50",
-      "Slide 3": "0:01:30"
+      "Slide 1": ": 0:00:00",
+      "Slide 2": ": 0:00:50",
+      "Slide 3": ": 0:01:30"
     }
     }
 ]
