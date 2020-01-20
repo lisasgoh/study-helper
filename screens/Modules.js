7fb2020fb52a8fc38bb8bb19b27d0860441dcbdc
GoldenCorgi <42860714+GoldenCorgi@users.noreply.github.com>
2020-01-20 09:51:48 +0800

    better aesthetics on modules

--- screens/Modules.js
+++ screens/Modules.js
@@ -1,45 +1,63 @@
 import React, { Component } from 'react';
 //import { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import { Button } from 'react-native-elements'
+import { Button, ListItem } from 'react-native-elements'
+
 //import {createAppContainer} from 'react-navigation';
 //import {createStackNavigator} from 'react-navigation-stack';
-const modules = ["CZ2002", "CZ2003", "CZ2004"]
+const modules = {"CZ2002":"Object Oriented Design and Programming", 
+"CZ2003":"Computer Graphics and Visualisation", 
+"CZ2004":"Human Computer Interaction"}
 
 export class Modules extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: modules, //if {} => object, now is array of json objects
+      data: Object.entries(modules), //if {} => object, now is array of json objects
+      
     };
   }
-    render() {
-      const {navigate} = this.props.navigation;
-      return (
-        <View style={styles.screen}>
-          {this.state.data.map((mod) => <Button title = {mod} containerStyle={styles.inputContainer} onPress={() => navigate('Search')} />)}
-        </View>
-      );
-    }
-  }
-
-  const styles = StyleSheet.create({
-    screen: {
-      padding: 20
-      
-    },
-    inputContainer: {
-        //flexDirection: 'row',
-        marginTop: 50,
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.screen}>
         
-        //justifyContent: 'space-between',
-        //alignItems: 'center'
-    },
-    input: {
-        width: '80%',
-        borderColor: 'black',
-        borderWidth: 1,
-        padding: 10
-    }
-  })
-  export default Modules;
+        {
+          this.state.data.map((x) => (
+            <ListItem style={styles.list}
+              onPress={() => navigate('Search')}
+              title={x[0]}
+              subtitle={x[1]}
+              bottomDivider
+              chevron
+            />
+          ))
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20
+
+  },
+  inputContainer: {
+    //flexDirection: 'row',
+    marginTop: 50,
+
+    //justifyContent: 'space-between',
+    //alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  list: {
+    marginTop:20,
+  }
+})
+export default Modules;
